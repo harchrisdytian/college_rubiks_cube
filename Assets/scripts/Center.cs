d068197e6c3788432abaa6bb9ae113aa58da78d0
harchrisdytian <harchrisdytian@gmail.com>
2022-03-29 10:47:23 -0400

    Stuff

---
+++ Assets/scripts/Center.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center : MonoBehaviour
+{
+    public GameObject[] piece;
+
+    enum PIECE_DIR { NE,N,NW,W,SW,SE,E}
+
+
+
+}
---
+++ Assets/scripts/Edge.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour
+{
+    public Center[] centers;
+
+
+    public void setCenter(Center Rotating_center,Center new_center)
+    {
+        centers[0];
+       
+    }
+}
